3608a90b74875d1371e1fb22ef73addb3b9b0e99
Sabio-JeffJ <jeffrey0061@gmail.com>
2015-06-07 19:10:22 -0700

    This is the entire project for my sample site

---
+++ PracticeSite/PracticeSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PracticeSite.MvcApplication" Language="C#" %>
---
+++ PracticeSite/PracticeSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PracticeSite.Startup))]
+namespace PracticeSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
